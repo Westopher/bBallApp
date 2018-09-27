a88bc22aeaa4e38815735cf16cadacb3b90158f2
West Kraemer <kraemerwest@gmail.com>
2018-09-27 17:32:49 -0400

    add IB actions for league vc buttons. Also, make next button on same VC disabled.

--- bBallApp/Controller/LeagueVC.swift
+++ bBallApp/Controller/LeagueVC.swift
@@ -10,6 +10,19 @@ import UIKit
 
 class LeagueVC: UIViewController {
 
+    @IBOutlet weak var nextBtn: Border_Button!
+    
+    @IBAction func onMensTapped(_ sender: Any) {
+    }
+    
+    @IBAction func onWomensTapped(_ sender: Any) {
+    }
+    
+    @IBAction func onCoedTapped(_ sender: Any) {
+    }
+    
+    
+    
     @IBAction func onNextTapped(_ sender: Any) {
         performSegue(withIdentifier: "skillVCSegue", sender: self)
     }
